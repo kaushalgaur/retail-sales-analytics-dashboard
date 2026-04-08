42c12a81b20a0c65c1b9c7991318b70aea50e68c
Kaushal Gaur <150648573+kaushalgaur@users.noreply.github.com>
2026-04-08 16:03:29 +0530

    Delete sql/6.Top Customers by Revenue.sql

--- sql/6.Top Customers by Revenue.sql
+++
@@ -1,9 +0,0 @@
-# 6.Top Customers by Revenue
-SELECT
-CustomerID,
-ROUND(SUM(Revenue),2) AS total_spent
-FROM retail_transactions
-WHERE CustomerID IS NOT NULL
-GROUP BY CustomerID
-ORDER BY total_spent DESC
-LIMIT 10;
